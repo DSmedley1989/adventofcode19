530b4cc6424cc4367fdb87518e32725abf15fc7d
Daniel Smedley <daniel.smedley@footballradar.com>
2019-12-11 09:18:29 +0000

    Final ten part 2
    After some horrendous debugging (NEVER apply a coordinate
    transformation in haste) finally got the laser to destroy
    the _right_ asteroids.

--- ten_b.py
+++ ten_b.py
@@ -69,7 +69,7 @@ class Asteroid:
 
 def convert_to_2d_list(input_string):
     return [
-        [char for char in row.strip()] for row in input_string.split("\n")
+        [char for char in row.strip()] for row in input_string.strip().split("\n")
     ]
 
 def extract_asteroids(two_dim_list):
@@ -117,7 +117,7 @@ unique_lines_of_sight = []
 laser_base = None
 targets = []
 
-string_field = TEST_MAP
+string_field = INPUT_MAP
 
 if "X" in string_field:
     laser_base, targets = extract_targets_directly(convert_to_2d_list(string_field))
@@ -137,51 +137,46 @@ else:
             laser_base = asteroid
             targets = [asteroid for asteroid in relative_asteroids if asteroid.not_at_origin()]
 
-
+# Sort our targets by radius so closest get destroyed first
 targets.sort(key=get_radius)
 
-print(str([target.get_r() for target in targets]))
-
-normalised_los = [los for los in unique_lines_of_sight]
-
-normalised_los.sort()
+# Our thetas are in fact increasing anticlockwise
+unique_lines_of_sight.sort(reverse=True)
 
 rotated_lines_of_sight = []
-
-for i, los in enumerate(normalised_los):
-    if los > (-math.pi / 2):
-        loc_pi_and_greater = normalised_los[:i]
-        loc_lt_pi = normalised_los[i:]
-
-        rotated_lines_of_sight.extend(loc_lt_pi)
-        rotated_lines_of_sight.extend(loc_pi_and_greater)
-        break
-
-denormalised_los = [los for los in rotated_lines_of_sight]
 
 last_target_destroyed = None
 targets_destroyed = 0
 
 print("Laser Base at %d, %d" % (laser_base.x, laser_base.y))
 print("%d targets visible" % len(unique_lines_of_sight))
+print("Targets (in order of distance from base):")
+print("\n".join(["(%d, %d) at theta %.6f" % (target.x, target.y, target.get_theta()) for target in targets]))
+
+print("Logic test")
+test_first_theta = unique_lines_of_sight[0]
+all_targets_with_matching_theta = [target for target in targets if target.get_theta() == test_first_theta]
+print("All targets along line of sight %.6f:" % test_first_theta)
+print("\n".join(["(%d, %d)" % (target.x, target.y) for target in all_targets_with_matching_theta]))
 
 while targets_destroyed < 200:
-    los = denormalised_los.pop(0)
+    los = unique_lines_of_sight.pop(0)
     re_add_los = True
 
     for i, target in enumerate(targets):
         if target.get_theta() == los:
             targets.pop(i)
             abs_target = target.detranslate(laser_base)
-            print("Target destroyed at %d, %d" % (abs_target.x, abs_target.y))
             last_target_destroyed = target
             targets_destroyed += 1
+            if targets_destroyed in [1,2,3,10,20,50,100,199,200]:
+                print("%dth Target destroyed at %d, %d" % (targets_destroyed, abs_target.x, abs_target.y))
             break
     else:
         re_add_los = False
 
     if re_add_los:
-        denormalised_los.append(los)
+        unique_lines_of_sight.append(los)
 
 absolute_target = last_target_destroyed.detranslate(laser_base)
 
